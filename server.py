5e92bf7177cfeab4256bfaefdb71843cc98b4d87
Daniel <daniel@gmail.com>
2023-10-27 10:34:24 +0300

    update

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from main import get_current_weather
 from waitress import serve
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import sqlite3
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
